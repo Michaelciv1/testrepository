e3cf7bf5409228874958f84558a2154a1cdce6c8
Michael Wallerius <mikesterciv1@gmail.com>
2020-10-01 17:55:19 -0700

    created

---
+++ lab_1.py
@@ -0,0 +1,19 @@
+"""
+CIS 40 Lab 1 - My First Python Program
+"""
+name = "Michael Wallerius"
+e_mail = "mikesterciv1@gmail.com"
+major = "Computer Science"
+experience = "I have finished the CIS 22 track"
+num1 = 54
+num2 = 17
+result = num1 * num2
+
+print("STUDENT NAME: " + name)
+print("E-MAIL: " + e_mail)
+print("MAJOR: " + major)
+print("COMPUTER PROGRAMMING EXPERIENCE: " + experience)
+print(str(num1) + "*" + str(num2))
+print(result)
+
+print("The computer can compute: ")
